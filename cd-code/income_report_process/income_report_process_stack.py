ebe8cec7ed676b53da8f43ffbab7c1ded3332fea
Avi Zelovich <avizelovich@Avis-MacBook-Air.local>
2026-04-04 17:36:10 +0300

    Try bucket_arn parameter for S3EventSource
    - Use bucket_arn instead of bucket parameter
    - This should pass the correct type to S3EventSource
    - S3EventSource expects IBucket interface, bucket_arn should provide it

--- cd-code/income_report_process/income_report_process_stack.py
+++ cd-code/income_report_process/income_report_process_stack.py
@@ -82,7 +82,7 @@ class IncomeReportProcessStack(Stack):
 
         # Add S3 event trigger for CSV files
         s3_trigger = _lambda_event_sources.S3EventSource(
-            bucket=csv_bucket,
+            bucket_arn=csv_bucket.bucket_arn,
             events=[s3.EventType.OBJECT_CREATED],
             filters=[s3.NotificationKeyFilter(prefix="", suffix=".csv")]
         )
